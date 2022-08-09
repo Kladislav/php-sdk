ed37aa3d3e57170e5e6267a5dcacc4607732d35f
kladislav <kim@mediacube.network>
2022-08-09 14:40:31 +0300

    add $governmentIdType

--- src/Hyperwallet/Model/BankAccount.php
+++ src/Hyperwallet/Model/BankAccount.php
@@ -57,6 +57,7 @@ namespace Hyperwallet\Model;
  * @property string $mobileNumber The mobile number
  *
  * @property string $governmentId The government id
+ * @property string $governmentIdType The government id type
  *
  * @property string $addressLine1 The address line 1
  * @property string $city The city
@@ -916,6 +917,26 @@ class BankAccount extends BaseModel {
      */
     public function setGovernmentId($governmentId) {
         $this->governmentId = $governmentId;
+        return $this;
+    }
+
+    /**
+     * Get the government id type
+     *
+     * @return string
+     */
+    public function getGovernmentIdType() {
+        return $this->governmentIdType;
+    }
+
+    /**
+     * Set the government id type
+     *
+     * @param string $governmentIdType
+     * @return BankAccount
+     */
+    public function setGovernmentIdType($governmentIdType) {
+        $this->governmentIdType = $governmentIdType;
         return $this;
     }
 
